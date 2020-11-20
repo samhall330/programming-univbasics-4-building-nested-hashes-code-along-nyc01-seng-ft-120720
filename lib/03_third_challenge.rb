40b3adfadbf5d3ba4fcf0193b6b47fd2b0ffc705
Sam Hall <mann.samantha11@gmail.com>
2020-11-20 20:12:56 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -6,17 +6,15 @@ def third_challenge
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
 
-
       ]
    },
-
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
-      :matriarch => {name: "Lady Capulet" age: "51"},
+      :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
 
-
+        
       ]
     }
   }
