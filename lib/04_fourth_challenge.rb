60ea75e0c7e377ee4633abb78995e085a591d4db
Sam Hall <mann.samantha11@gmail.com>
2020-11-20 20:16:11 +0000

    Automatically backed up by Learn

--- lib/04_fourth_challenge.rb
+++ lib/04_fourth_challenge.rb
@@ -12,7 +12,6 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [{name: "Mercutio", age: "18", attitude: "hot-headed"}
-        #your hashes here!
       ]
    }
 }
