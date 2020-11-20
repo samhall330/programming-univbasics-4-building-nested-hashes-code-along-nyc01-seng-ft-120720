a856f9883a8739b44e482eb57b8185f6a04df0ee
Sam Hall <mann.samantha11@gmail.com>
2020-11-20 20:16:02 +0000

    Automatically backed up by Learn

--- lib/04_fourth_challenge.rb
+++ lib/04_fourth_challenge.rb
@@ -4,14 +4,14 @@ def fourth_challenge
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
-      :hero_friends => [{name: "Benvolio", age: "18", attitude: "worried"}
+      :hero_friends => [{name: "Benvolio", age: "17", attitude: "worried"}
       ]
    },
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
-      :heroine_friends => [
+      :heroine_friends => [{name: "Mercutio", age: "18", attitude: "hot-headed"}
         #your hashes here!
       ]
    }
